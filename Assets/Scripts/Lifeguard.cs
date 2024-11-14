a242b79a16c6e847c01dc2e4afda431d9bfba454
SergioPerezRobledanodano <sepere12@ucm.es>
2024-11-14 11:58:11 +0100

    AnimacionesPlayer

--- Assets/Scripts/Lifeguard.cs
+++ Assets/Scripts/Lifeguard.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class Lifeguard : MonoBehaviour
 {
     private NavMeshAgent agent;
     private Camera mainCamera;
-
     private bool tieneBotiquin;
+    private bool drch=true;
+    private double dest = 0.0;
+    private Transform transform;
+    private SpriteRenderer sprite;
+    [SerializeField] private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        sprite = GetComponent<SpriteRenderer>();
+        transform = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
@@ -23,15 +31,34 @@ public class Lifeguard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         // Detectar si el clic izquierdo del ratón es presionado
         if (Input.GetMouseButtonDown(0))  // 0 corresponde al clic izquierdo
         {
             RaycastHit hit;
+            Debug.Log(agent.speed);
             Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
+                animator.SetBool("Moving", true);
                 agent.SetDestination(hit.point);
+                dest = hit.point.x;
+                Debug.Log(hit.point.x);
+                sprite.flipX = hit.point.x < transform.position.x;
+            }
+
+        }
+        else
+        {
+            Debug.Log(dest + " "+transform.position.x);
+            if (transform.position.x == dest)
+            {
+                animator.SetBool("Moving", false);
+
             }
         }
+ 
+
+
     }
 }
